54613060b4452e6b5abcadf0b0f70df4e54137e7
Jarrod Watts <jarrodwatts16@gmail.com>
2021-06-04 12:55:38 +1000

    Add Google Analytics

--- src/pages/api/github.ts
+++ src/pages/api/github.ts
@@ -5,14 +5,17 @@ import Repository from "../../types/Repository";
  * GitHub API.
  * Returns a list of public repositiories from my GitHub Profile.
  */
-export default async (_: NextApiRequest, res: NextApiResponse<Repository[]>) => {
+export default async (
+  _: NextApiRequest,
+  res: NextApiResponse<Repository[]>
+) => {
   try {
     // TODO: This uses the public API. Which gets rate limited badly. Should swap to oauth
     // https://octokit.github.io/rest.js/v18#repos-list-for-user
     const apiUrl = "https://api.github.com/users/jarrodwatts/repos";
-    const ytRes = await fetch(apiUrl);
-    const ytData = await ytRes.json();
-    res.status(200).json(ytData as Repository[]);
+    const ghRes = await fetch(apiUrl);
+    const ghData = await ghRes.json();
+    res.status(200).json(ghData as Repository[]);
   } catch (error) {
     // The error is handled by the client-side by destructuring error from the fetcher response
     console.error("Error occurred returning YouTube API data");
