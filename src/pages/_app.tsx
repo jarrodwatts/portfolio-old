9e70291ed32c156ea3d05d806db37cd9bcfd98c3
the intern <jarrodwatts16@gmail.com>
2021-06-28 13:30:44 +1000

    Upgrade to Next 11, use script component for Google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
 import Header from "../components/Header";
 import Container from "@material-ui/core/Container";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps): any {
   useEffect(() => {
@@ -18,6 +19,23 @@ export default function MyApp({ Component, pageProps }: AppProps): any {
 
   return (
     <React.Fragment>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+
       <Head>
         <title>Jarrod Watts</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,24 +8,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
